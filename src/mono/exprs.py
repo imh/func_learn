21483e646125d4e22a1f22fd7d06903bab3db848
Ian Horn <horn.imh@gmail.com>
2016-07-13 17:13:31 -0700

    Changed baseline for gradients. It works now!!!!

--- src/mono/exprs.py
+++ src/mono/exprs.py
@@ -34,7 +34,8 @@ spec['rules'] = [var,
 eval_spec = dict(spec)
 eval_spec['rules'] = eval_spec['rules'] + [app, bool_expr, int_expr]
 
-np.random.seed(1)
+# np.random.seed(3) #good for dim=10
+np.random.seed(1) #good for dim=5
 
 class ProgModel(Chain):
     def __init__(self, n_progs, dim):
@@ -49,7 +50,7 @@ class ProgModel(Chain):
             state2var=L.Linear(dim, dim),
             state2var_choice=L.Linear(dim, dim)
             )
-model = ProgModel(n_progs=1, dim=10)
+model = ProgModel(n_progs=1, dim=5)
 optimizer = optimizers.SGD(lr=0.01)
 optimizer.setup(model)
 optimizer.add_hook(chainer.optimizer.GradientClipping(20.0))
@@ -70,7 +71,7 @@ class LossComputer(object):
 i = 0
 while True:
     i += 1
-    print 'starting epoch {}'.format(i)
+    print 'epoch {},'.format(i),
     tries = 0
     context = dict()
     context['model'] = model
@@ -81,7 +82,7 @@ while True:
     optimizer.zero_grads()
     n_ok = 0
     tries = 0
-    while not (n_ok >= 100 and tries >= 300):
+    while not (n_ok >= 100 and tries >= 100):
         context['depth'] = 0
         context['state'] = model.init_state(Variable(cuda.to_gpu(np.array([0], dtype=np.int32))))
         context['lp'] = 0
@@ -111,20 +112,29 @@ while True:
         max_ok_errs = np.max(ok_err_means)
         min_ok_errs = np.min(ok_err_means)
         not_ok_err = max_ok_errs + (max_ok_errs - min_ok_errs) + 500
+        mean_errs = np.mean([np.mean(lc.errs) if lc.ok else not_ok_err for lc in lcs])
         if max_ok_errs > 5e3:
             scal = 1.0 #5e3 / max_ok_errs
         else:
             scal = 1.0
         # print 'computed errs'
         loss = Variable(cuda.to_gpu(np.array([0], dtype=np.float32)))
+        loss2 = Variable(cuda.to_gpu(np.array([0], dtype=np.float32)))
+        avg_err = 0
         # print 'adding losses'
         n_ok = len([lc for lc in lcs if lc.ok])
         for lc in lcs:
             err = np.mean(lc.errs) if lc.ok else not_ok_err
-            loss += float(err - mean_ok_errs) * lc.lp
+            loss += float(err - mean_errs) * lc.lp
+            loss2 += float(err) * lc.lp
+            avg_err += float(err)
         loss /= len(lcs)
+        loss2 /= len(lcs)
+        avg_err /= len(lcs)
 
         # print 'added losses'
-        print 'LOSS_OK (mean={}, max={}):'.format(mean_ok_errs, max_ok_errs), '{} / {} ok.'.format(len([lc for lc in lcs if lc.ok]), len(lcs)),
+        print 'LOSS_OK (min={:.1f}, mean={:.1f}, max={:.1f}),'.format(min_ok_errs, mean_ok_errs, max_ok_errs), '{} / {} ok.'.format(len([lc for lc in lcs if lc.ok]), len(lcs)),
         loss.backward()
+        print '|grad|_2: {:.4E},'.format(optimizer.compute_grads_norm()),
+        print 'LOSS={:.4E}, LOSS2={:.4E}, avg_err={:.4E}'.format(loss.data.get()[0], loss2.data.get()[0], avg_err)
         optimizer.update()
